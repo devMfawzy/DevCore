07def70ea3370335e171b50f08bfa187984dfd70
devMfawzy <m.fawzy.a@icloud.com>
2022-08-28 03:10:25 +0200

    ass seconf networking API

--- Sources/DevCore/Networking.swift
+++ Sources/DevCore/Networking.swift
@@ -9,11 +9,19 @@ import Foundation
 
 protocol NetworkSession {
     func get(from url: URL, completion: @escaping ((Data?, Error?) -> Void))
+    func post(with request: URLRequest, completion: @escaping ((Data?, Error?) -> Void))
 }
 
 extension URLSession: NetworkSession {
     func get(from url: URL, completion: @escaping ((Data?, Error?) -> Void)) {
         let task = dataTask(with: url) { data, _, error in
+            completion(data, error)
+        }
+        task.resume()
+    }
+    
+    func post(with request: URLRequest, completion: @escaping ((Data?, Error?) -> Void)) {
+        let task = dataTask(with: request) { data, _, error in
             completion(data, error)
         }
         task.resume()
@@ -36,6 +44,22 @@ extension DevCore {
             ///   - completion: the block of result called on completion either with data or error
             public func loadData(from url: URL, completion: @escaping (NetworkResult<Data>) -> Void) {
                 session.get(from: url) { data, error in
+                    let result = data.map(NetworkResult<Data>.success) ?? .failure(error)
+                    completion(result)
+                }
+            }
+            
+            
+            /// call the live internet to send data to a specific location
+            /// - Parameters:
+            ///   - url: the location to fetch fata from
+            ///   - body: an object that confirms to emcodable protocol
+            ///   - completion: e block of result called on completion either with data or error 
+            public func sendData<I:Encodable>(to url: URL, body: I, completion: @escaping (NetworkResult<Data>) -> Void) {
+                var request = URLRequest(url: url)
+                request.httpMethod = "POST"
+                request.httpBody = try? JSONEncoder().encode(body)
+                session.post(with: request) { data, error in
                     let result = data.map(NetworkResult<Data>.success) ?? .failure(error)
                     completion(result)
                 }
--- Tests/DevCoreTests/DevNetworkingTests.swift
+++ Tests/DevCoreTests/DevNetworkingTests.swift
@@ -15,6 +15,15 @@ class NetworkSessionMock: NetworkSession {
     func get(from url: URL, completion: @escaping ((Data?, Error?) -> Void)) {
         completion(data, error)
     }
+    
+    func post(with request: URLRequest, completion: @escaping ((Data?, Error?) -> Void)) {
+        completion(data, error)
+    }
+}
+
+struct MockData: Codable, Equatable {
+    let id: Int
+    let name: String
 }
 
 final class DevNetworkingTests: XCTestCase {
@@ -38,7 +47,30 @@ final class DevNetworkingTests: XCTestCase {
         wait(for: [expectaion], timeout: 5)
     }
 
+    func testSendDataCall() {
+        let session = NetworkSessionMock()
+        let manager = DevCore.Networking.Manager()
+        let sampleObject = MockData(id: 1, name: "Adam")
+        let data = try? JSONEncoder().encode(sampleObject)
+        session .data = data
+        manager.session = session
+        let url = URL(fileURLWithPath: "url---")
+        let expectaion = XCTestExpectation(description: "called for data")
+        manager.sendData(to: url, body: sampleObject) { result in
+            expectaion.fulfill()
+            switch result {
+            case .success(let resultData):
+                let resultObject = try? JSONDecoder().decode(MockData.self, from: resultData)
+                XCTAssertEqual(resultObject, sampleObject, "manager returned unexpected object ")
+            case .failure(let error):
+                XCTFail(error?.localizedDescription ?? "error forming error result")
+            }
+        }
+        wait(for: [expectaion], timeout: 5)
+    }
+    
     static var allTests = [
-        ("testLoadDataCall", testLoadDataCall)
+        ("testLoadDataCall", testLoadDataCall),
+        ("testSendDataCall", testSendDataCall)
     ]
 }
